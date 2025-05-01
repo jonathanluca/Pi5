005f8995e143db50b8193a14ae59f2915d7c43c5
Jonathan Lucas <jonathanlsantos.m@gmail.com>
2025-04-30 22:38:11 -0300

    Alteração nas telas

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
     try:
-        from django.core.management import execute_from_command_line
+        import django  # Verifica se o Django está instalado
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
